405c50c8991f9553c73c8980ef1d25c59911edb1
Amphakarn <pisuthigomol@yahoo.com>
2021-10-31 17:08:43 +0000

    Create locators test file

--- project-1/cypress/integration/test-project-1/locators.spec.js
+++ project-1/cypress/integration/test-project-1/locators.spec.js
@@ -1,1 +1,11 @@
 /// <reference types="cypress" />
+
+describe('Locators', () => {
+  beforeEach(() => {
+    cy.visit('/elements'); // pick up url based on the "baseUrl" key in cypress.json file
+  });
+
+  it('locating elements with get', () => {});
+});
+
+//test
